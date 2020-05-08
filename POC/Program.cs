3684c389aee7c2e82cc0a7906c553a86ecf840fa
Deepak Bhagat <bhagatdeepak95@gmail.com>
2020-05-08 17:19:22 +0530

    Test

--- POC/Program.cs
+++ POC/Program.cs
@@ -9,7 +9,7 @@ namespace POC
         {
             JObject ObjSon = new JObject();
             string Jstring = @"{
-                                  'PaymentMode': 'DCB',
+                                  'PaymentMode': 'DCBf',
                                   'TotalInvestAMount': '5',
                                   'PG': 'BillDesk',
                                   'Campaign': 'ADDTIONALPURCHASE',
