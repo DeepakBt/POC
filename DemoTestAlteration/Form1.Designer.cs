ae734e48942e90bad995b537c989d7a8efa9d0e2
Deepak Bhagat <bhagatdeepak95@gmail.com>
2020-09-20 12:39:00 +0530

    AlterationProj

---
+++ DemoTestAlteration/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DemoTestAlteration
+{
+    partial class SearchAlteration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnFillData = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Text : ";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(93, 10);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(200, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnFillData
+            // 
+            this.btnFillData.Location = new System.Drawing.Point(93, 36);
+            this.btnFillData.Name = "btnFillData";
+            this.btnFillData.Size = new System.Drawing.Size(92, 23);
+            this.btnFillData.TabIndex = 2;
+            this.btnFillData.Text = "Fill Data";
+            this.btnFillData.UseVisualStyleBackColor = true;
+            this.btnFillData.Click += new System.EventHandler(this.btnFillData_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(191, 36);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(102, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search Text";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 65);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(281, 150);
+            this.dataGrid.TabIndex = 4;
+            // 
+            // SearchAlteration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 227);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnFillData);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(321, 266);
+            this.MinimumSize = new System.Drawing.Size(321, 266);
+            this.Name = "SearchAlteration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alteration";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnFillData;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ DemoTestAlteration/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+namespace DemoTestAlteration
+{
+    public partial class SearchAlteration : Form
+    {
+        public SearchAlteration()
+        {
+            InitializeComponent();
+        }
+        DataTable dt;
+        DataTable dt2;
+        DataTable dt3;
+        bool isFound1 = false;
+        bool isFound2 = false;
+        string Str1 = string.Empty;
+        string Str2 = string.Empty;
+        private void btnFillData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int no = Convert.ToInt32(txtSearch.Text);
+                int Partition = no / 2;
+                if (no <=0 || no>1000000)
+                {
+                    MessageBox.Show("Fill Data Can Not Be Greated in 1000000");
+                }
+                else
+                {
+                    dt = new DataTable();
+                    dt.Columns.Add(new DataColumn("Id", typeof(int)));
+                    dt.Columns.Add(new DataColumn("Name", typeof(string)));
+                    dt2 = new DataTable();
+                    dt2.Columns.Add(new DataColumn("Id", typeof(int)));
+                    dt2.Columns.Add(new DataColumn("Name", typeof(string)));
+                    for (int i = 1; i <= no; i++)
+                    {
+                        if (i <= Partition)
+                        {
+                            DataRow dr = dt.NewRow();
+                            dr["Id"] = i;
+                            dr["Name"] = Path.GetRandomFileName().Replace(".", "");
+                            dt.Rows.Add(dr);
+                        }
+                        else
+                        {
+                            DataRow dr = dt2.NewRow();
+                            dr["Id"] = i;
+                            dr["Name"] = Path.GetRandomFileName().Replace(".", "");
+                            dt2.Rows.Add(dr);
+                        }
+                    }
+                    dt3 = new DataTable();
+                    dt3 = dt.Copy();
+                    dt3.Merge(dt2);
+                    dataGrid.DataSource = dt3;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter number between 1 to 1000000");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            isFound1 = false;
+            isFound2 = false;
+            Str1 = string.Empty;
+            Str2 = string.Empty;
+            Search2();
+        }
+        public void Search()
+        {
+            if (txtSearch.Text.Length == 0)
+            {
+                MessageBox.Show("Search text can not be blank");
+            }
+            else if (dt.Rows.Count <= 0)
+            {
+                MessageBox.Show("Please fill the data first");
+            }
+            else
+            {
+                {
+                    Stopwatch watch = new Stopwatch();
+                    watch.Start();
+                    var results = from myRow in dt.AsEnumerable()
+                                  where myRow.Field<string>("Name") == txtSearch.Text
+                                  select myRow;
+                    if (results.ToList().Count > 0)
+                    {
+                        watch.Stop();
+                        MessageBox.Show(string.Format("Text {0} Found at position {1} in ms {2}", txtSearch.Text, results.ToList()[0].ItemArray[0].ToString(), watch.ElapsedMilliseconds));
+                    }
+                    else
+                    {
+                        MessageBox.Show(string.Format("Text {0} Not Found", txtSearch.Text));
+                    }
+                }
+            }
+        }
+        public void Search2()
+        {
+            if (txtSearch.Text.Length == 0)
+            {
+                MessageBox.Show("Search text can not be blank");
+            }
+            else if (dt.Rows.Count <= 0)
+            {
+                MessageBox.Show("Please fill the data first");
+            }
+            else
+            {
+                {
+                    string SearchText = txtSearch.Text;
+                    Thread thread = new Thread(() => searchThread1(SearchText));
+                    thread.Start();
+                    Thread thread2 = new Thread(() => searchThread2(SearchText));
+                    thread2.Start();
+                    thread.Join();
+                    thread2.Join();
+
+                    if (isFound1)
+                    {
+                        MessageBox.Show(Str1);
+                    }else if(isFound2)
+                    {
+                        MessageBox.Show(Str2);
+                    }else
+                    {
+                        MessageBox.Show(string.Format("Text {0} Not Found", txtSearch));
+                    }
+                }
+            }
+        }
+        public void searchThread1(string txtSearch)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var results = from myRow in dt.AsEnumerable()
+                          where myRow.Field<string>("Name") == txtSearch
+                          select myRow;
+            if (results.ToList().Count > 0)
+            {
+                isFound1 = true;
+                watch.Stop();
+                Str1 = string.Format("Text {0} Found at position {1} in ms {2}", txtSearch, results.ToList()[0].ItemArray[0].ToString(), watch.ElapsedMilliseconds);
+            }
+        }
+        public void searchThread2(string txtSearch)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var results = from myRow in dt2.AsEnumerable()
+                          where myRow.Field<string>("Name") == txtSearch
+                          select myRow;
+            if (results.ToList().Count > 0)
+            {
+                isFound2 = true;
+                watch.Stop();
+                Str2 = string.Format("Text {0} Found at position {1} in ms {2}", txtSearch, results.ToList()[0].ItemArray[0].ToString(), watch.ElapsedMilliseconds);
+            }
+        }
+    }
+}
