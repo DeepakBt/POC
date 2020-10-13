308c92dc530210ed9582405ea38adda7bf7f4a3c
Deepak Bhagat <bhagatdeepak95@gmail.com>
2020-10-13 18:38:04 +0530

    GetWebHTML

---
+++ GetWebHTML/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GetWebHTML
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+            string HtmlData =  await client.GetStringAsync("https://enetbanking.hdfcbank.com/corporate/CorporateLogin.html");
+            Console.WriteLine(HtmlData);
+            Console.ReadKey();
+        }
+    }
+}
