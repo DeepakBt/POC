d171b6d519a00e846ffb11b86a8c585189588f22
Deepak Bhagat <bhagatdeepak95@gmail.com>
2021-02-06 19:57:22 +0530

    Async

---
+++ Async/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Async Programming!");
+            Program ObjP = new Program();
+            ObjP.Meth1();
+            ObjP.Meth2();
+
+            //ObjP.Meth2();
+            //ObjP.Meth1();
+            Console.ReadLine();
+        }
+        public async void Meth1()
+        {
+            await Task.Run(()=>{
+            for(int i=1;i<=20;i++)
+                {
+                    Console.WriteLine($"{i} Async");
+                }
+            });
+        }
+        public void Meth2()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"{i} Sync");
+            }
+        }
+    }
+}
