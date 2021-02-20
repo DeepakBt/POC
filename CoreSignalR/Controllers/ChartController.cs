7900aa628c73d448c1c99a463e3f77b16345b6c7
Deepak Bhagat <bhagatdeepak95@gmail.com>
2021-02-20 23:02:19 +0530

    ChartSignalR

---
+++ CoreSignalR/Controllers/ChartController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CoreSignalR.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChartHub> _hub;
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+        public IActionResult GetData()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
