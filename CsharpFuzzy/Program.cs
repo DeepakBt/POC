95ff161848392871bac45210a45779bbd67d3124
Deepak Bhagat <bhagatdeepak95@gmail.com>
2021-07-08 17:39:29 +0530

    hw

---
+++ CsharpFuzzy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace CsharpFuzzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("MAC Address : "+ Convert.ToString(GetMacAddress()));
+            HDDInfo();
+            Console.ReadLine();
+        }
+        public static PhysicalAddress GetMacAddress()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && nic.OperationalStatus == OperationalStatus.Up && nic.Name.ToUpper()== "Ethernet".ToUpper())
+                {
+                    return nic.GetPhysicalAddress();
+                }
+            }
+            return null;
+        }
+
+        public static void HDDInfo()
+        {
+            System.Management.ManagementClass mgmtClass = new System.Management.ManagementClass("Win32_Service");
+            System.Management.ManagementObjectCollection mgmtObjCol = mgmtClass.GetInstances();
+
+            foreach (System.Management.ManagementObject MGMTOBJ in mgmtObjCol)
+            {
+                Console.WriteLine(MGMTOBJ.ToString());
+                foreach (System.Management.PropertyData property in MGMTOBJ.Properties)
+                {
+                    Console.WriteLine("Property Name: " + property.Name + "\tProperty Value: " + property.Value);
+                }
+            }
+        }
+    }
+}
