de552f315617dcf3539c0244bc810a81a852d6c5
Deepak Bhagat <bhagatdeepak95@gmail.com>
2021-08-24 20:52:37 +0530

    upload

--- UploadMVC4/Controllers/HomeController.cs
+++ UploadMVC4/Controllers/HomeController.cs
@@ -20,6 +20,46 @@ namespace UploadMVC4.Controllers
         [HttpPost]
         public async Task<ActionResult> Index(HttpPostedFileBase ReqExcelFile)
         {
+            //using (var client = new HttpClient())
+            //{
+            //    // using (var content = new MultipartFormDataContent())
+            //    //  {
+            //        var multipartContent = new MultipartFormDataContent();
+
+            //        byte[] Bytes = new byte[ReqExcelFile.InputStream.Length + 1];
+            //        ReqExcelFile.InputStream.Read(Bytes, 0, Bytes.Length);
+            //        var fileContent = new ByteArrayContent(Bytes);
+            //        //fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment") { FileName = ReqExcelFile.FileName };
+            //        //fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+            //        //multipartContent.Add(fileContent);
+            //        multipartContent.Add(fileContent, "XLSXFile",  ReqExcelFile.FileName);
+            //        client.DefaultRequestHeaders.Add("AppId", "41203F44M9E21A450CM9F97C5799B6471E43");
+            //        //client.DefaultRequestHeaders.Add("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            //        //client.DefaultRequestHeaders.Contains.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //        //var multipartContent = new MultipartFormDataContent();
+            //        //multipartContent.Add(byteArrayContent, "csvFile", "filename");
+            //        //var postResponse = await _client.PostAsync("offers", multipartContent);
+
+
+            //        var requestUri = "https://api.moamc.com/LMS/api/Sales/UploadAIFNav";
+            //        var result = client.PostAsync(requestUri, multipartContent).Result;
+            //        string strRes = result.Content.ReadAsStringAsync().Result;
+            //        //string response=result.Content.ReadAsStringAsync().ToString();
+            //        if (result.StatusCode == System.Net.HttpStatusCode.Created)
+            //        {
+            //            //List<string> m = result.Content.ReadAsAsync<List<string>>().Result;
+
+            //            //ViewBag.Success = m.FirstOrDefault();
+            //            ViewBag.Success = "Success";
+            //        }
+            //        else
+            //        {
+            //            ViewBag.Failed = "Failed !" + result.Content.ToString();
+            //        }
+            //   // }
+            //}
+
             using (var httpClient = new HttpClient())
             {
                 httpClient.DefaultRequestHeaders.Add("AppId", "41203F44M9E21A450CM9F97C5799B6471E43");
@@ -33,8 +73,9 @@ namespace UploadMVC4.Controllers
                             using (var fileContent = new ByteArrayContent(await streamContent.ReadAsByteArrayAsync()))
                             {
                                 fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+                                // "file" parameter name should be the same as the server side input parameter name
                                 form.Add(fileContent, "ReqExcelFile", ReqExcelFile.FileName);
-                                HttpResponseMessage response = await httpClient.PostAsync("https://*&UY^Yccdcdcdcdcd", form);
+                                HttpResponseMessage response = await httpClient.PostAsync("https://api.f.com/LMS/api/Sales/UploadAIFNav", form);
                                 string strResponse = response.Content.ReadAsStringAsync().Result;
                             }
                         }
